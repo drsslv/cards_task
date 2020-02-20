20771ea8821a6724cc7f951eb70f274db370223d
Andrei Salov <salov_97@mail.ru>
2020-02-21 01:30:38 +0300

    lol

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static byte Min(Queue<byte> que)
+        {
+            byte min = 101;
+
+            while (que.Count > 0)
+            {
+                byte element = que.Dequeue();
+                if (min > element) min = element;
+            }
+            return min;
+
+        }
+        
+        static Queue<byte> Copy(Queue<byte> cards)
+        {
+            var bytes = cards.ToArray();
+            return new Queue<byte>(bytes);
+        }
+        static void Main(string[] args)
+        {
+         
+            byte a = byte.Parse(Console.ReadLine());
+            Queue<byte> cards = new Queue<byte>();
+            var line = Console.ReadLine().Split(' ');
+            for (int i = 0; i < a; i++)
+            {
+                cards.Enqueue(byte.Parse(line[i]));
+
+            }
+            byte sum = 0;
+   
+            while (cards.Count > 0)
+            {
+                sum++;
+                var que = Copy(cards);
+                byte currentmin = Min(que);
+                if (currentmin == cards.Peek())
+                {
+                    cards.Dequeue();
+                }
+                else
+                {
+                    cards.Enqueue(cards.Peek());
+                    cards.Dequeue();
+                }
+            }
+            Console.WriteLine(sum);
+            
+
+        }
+    }
+}
